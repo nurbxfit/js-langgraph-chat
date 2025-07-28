64affc2faaabc913ae97c4ef15723f1cd1494576
nurbxfit <nurbxfit@gmail.com>
2025-07-28 12:52:56 +0800

    refactor just to waste time

--- index.ts
+++ index.ts
@@ -1,53 +1,8 @@
 
-import { chatPrompt } from "./src/prompts/basic";
 import { llm } from "./src/llm";
-import ChatIO from "./src/utils/chat-io";
-import { v4 as uuidv4 } from "uuid";
-import { END, MemorySaver, MessagesAnnotation, START, StateGraph } from "@langchain/langgraph";
+import ChatApp from "./src/app";
 
 
-const chat_id = uuidv4();
+const app = new ChatApp(llm);
 
-const io = new ChatIO();
-console.log('chat_id:', chat_id);
-
-// try using langraph to gives memory to the chat
-const graph = new StateGraph(MessagesAnnotation);
-
-// define the node graph
-graph.addNode("model", async (state: typeof MessagesAnnotation.State) => {
-    const prompt = await chatPrompt.invoke(state);
-    const response = await llm.invoke(prompt);
-
-    console.log('DEBUG:', response);
-
-    return { messages: [response] };
-})
-    .addEdge(START, "model")
-    .addEdge("model", END);
-
-
-// Add memory
-const memory = new MemorySaver();
-
-const app = graph.compile({ checkpointer: memory });
-const config = { configurable: { thread_id: uuidv4() } };
-
-
-console.log("Chat started. Type '/exit' to quit.");
-while (true) {
-    const input = await io.read();
-    if (!input) break;
-
-    const userInput = [
-        {
-            role: 'user',
-            content: input
-        }
-    ];
-    const response = await app.invoke({ messages: userInput }, config);
-    const lastMessage = response.messages[response.messages.length - 1]?.content;
-    // clean up remove the thinking part
-    const cleanedText = String(lastMessage).replace(/<think>[\s\S]*?<\/think>/, "").trim()
-    io.write(cleanedText);
-}
+app.run();
---
+++ src/app.ts
@@ -0,0 +1,95 @@
+import type { ChatOllama } from "@langchain/ollama";
+import ChatIO from "./utils/chat-io";
+import { v4 as uuidv4 } from "uuid";
+import { BinaryOperatorAggregate, CompiledStateGraph, END, MemorySaver, MessagesAnnotation, START, StateGraph, type Messages, type StateType } from "@langchain/langgraph";
+import { chatPrompt } from "./prompts/basic";
+import type { BaseMessage } from "@langchain/core/messages";
+
+interface ChatConfig {
+    configurable: { thread_id: string };
+}
+
+interface ChatState {
+    messages: BaseMessage[];
+}
+
+class ChatApp {
+    protected io!: ChatIO;
+    protected app!: CompiledStateGraph<ChatState, any>;
+    protected config!: ChatConfig;
+    constructor(protected llm: ChatOllama, protected id: string = uuidv4()) {
+        this.init();
+    }
+
+    init() {
+        // create a memory server
+        const memory = new MemorySaver();
+        // init IO
+        this.io = new ChatIO();
+        // try using langraph to gives memory to the chat
+        const graph = new StateGraph(MessagesAnnotation);
+
+        // define the node graph
+        graph
+            .addNode("model", (state) => this.modelHandler(state))
+            .addEdge(START, "model")
+            .addEdge("model", END);
+
+        this.app = graph.compile({ checkpointer: memory });
+        this.config = { configurable: { thread_id: uuidv4() } };
+
+    }
+
+    protected async modelHandler(state: typeof MessagesAnnotation.State) {
+        const prompt = await chatPrompt.invoke(state); // chatPrompt =  ChatPromptTemplate.fromMessages
+        const response = await this.llm.invoke(prompt);
+
+        return { messages: [response] };
+    }
+
+
+    async listenInput() {
+        const input = await this.io.read();
+        if (!input) return undefined;
+        return [
+            {
+                role: 'user',
+                content: input
+            }
+        ]
+    }
+
+    writeOutput(data: StateType<{
+        messages: BinaryOperatorAggregate<BaseMessage[], Messages>;
+    }>) {
+        const lastMessage = data.messages[data.messages.length - 1]?.content;
+        const cleanedText = String(lastMessage).replace(/<think>[\s\S]*?<\/think>/, "").trim();
+        this.io.write(cleanedText)
+    }
+
+
+    async run() {
+        while (true) {
+            try {
+                const input = await this.listenInput();
+                if (!input) break;
+
+                const response = await this.app.invoke({ messages: input }, this.config);
+                // Ensure response has the correct shape for writeOutput
+                if (response && Array.isArray(response.messages)) {
+                    this.writeOutput(response as StateType<{ messages: BinaryOperatorAggregate<BaseMessage[], Messages>; }>);
+                } else {
+                    throw new Error("Response does not contain messages array.");
+                }
+            } catch (error) {
+                console.error("Chat error:", error);
+                this.io.write("Error occured");
+                process.exit(1);
+            }
+
+        }
+    }
+}
+
+
+export default ChatApp;
