1d0e47ad839d7aeed43e121060be1a0f43c5020d
nurbxfit <nurbxfit@gmail.com>
2025-07-28 13:20:33 +0800

    add a comments a little bit

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 
 import { llm } from "./src/llm";
-import ChatApp from "./src/app";
+import ChatApp, { type ChatConfig } from "./src/app";
+// import { v4 as uuidv4 } from 'uuid';
 
+// can be usefull when we implement persistence memory
+// const id = "00b33e0e-5cf0-441d-81f7-231b4e0a66dc" // uuidv4();
+// // console.log('id:', id);
 
+// const config: ChatConfig = {
+//     configurable: {
+//         thread_id: id
+//     }
+// }
 const app = new ChatApp(llm);
 
 app.run();
